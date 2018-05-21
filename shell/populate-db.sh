91f89e0fca6b8abf0ce69db8835e4c9d2a6c9ca6
sgalopin <sg@griffedartistes.com>
2018-05-21 10:43:02 +0200

    setup vm for travis

--- shell/populate-db.sh
+++ shell/populate-db.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
+set -o pipefail
+
 echo "--------------------------------------------------"
 echo " Populate DB "
 echo "--------------------------------------------------"
---
+++ shell/setup-vm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+echo "--------------------------------------------------"
+echo " Setup the VM "
+echo "--------------------------------------------------"
+
+# Port 80 requires elevated privileges
+# https://docs.requarks.io/wiki/troubleshooting#error-listening-on-port-xx-requires-elevated-privileges
+apt-get install -y libcap2-bin
+setcap 'cap_net_bind_service=+ep' `which node`
+
+# MongoDB
+apt-get install -y mongodb
+sed -i 's/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g' /etc/mongodb.conf
+service mongodb restart
+
+# Setup the database
+sh ./populate-db.sh
