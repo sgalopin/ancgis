9956e7e5039eb799edcb2a2b0ec909988bd5812f
sgalopin <sg@griffedartistes.com>
2018-05-21 15:03:37 +0200

    travis conf

--- shell/setup-vm.sh
+++ shell/setup-vm.sh
@@ -7,9 +7,12 @@ echo " Setup the VM "
 echo "--------------------------------------------------"
 
 echo "-- Port 80 requires elevated privileges "
+# https://www.digitalocean.com/community/tutorials/how-to-use-pm2-to-setup-a-node-js-production-environment-on-an-ubuntu-vps
 # https://docs.requarks.io/wiki/troubleshooting#error-listening-on-port-xx-requires-elevated-privileges
+# https://stackoverflow.com/questions/16573668/best-practices-when-running-node-js-with-port-80-ubuntu-linode
 #sudo apt-get install -y libcap2-bin
 #sudo setcap 'cap_net_bind_service=+ep' `which node`
+sudo setcap cap_net_bind_service=+ep /usr/local/bin/node
 
 echo "-- MongoDB "
 #sudo apt-get install -y mongodb
