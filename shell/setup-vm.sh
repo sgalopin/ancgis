552119b3873fce9f0d1b528055e51097b67f8348
sgalopin <sg@griffedartistes.com>
2018-05-21 12:14:10 +0200

    travis conf

--- shell/setup-vm.sh
+++ shell/setup-vm.sh
@@ -6,15 +6,12 @@ echo "--------------------------------------------------"
 echo " Setup the VM "
 echo "--------------------------------------------------"
 
-echo " Port 80 requires elevated privileges "
+echo "-- Port 80 requires elevated privileges "
 # https://docs.requarks.io/wiki/troubleshooting#error-listening-on-port-xx-requires-elevated-privileges
-sudo apt-get install -y libcap2-bin
+#sudo apt-get install -y libcap2-bin
 sudo setcap 'cap_net_bind_service=+ep' `which node`
 
-echo " MongoDB "
-sudo apt-get install -y mongodb
-sudo sed -i 's/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g' /etc/mongodb.conf
-sudo service mongodb restart
-
-echo " Setup the database "
-/bin/bash populate-db.sh
+echo "-- MongoDB "
+#sudo apt-get install -y mongodb
+#sudo sed -i 's/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g' /etc/mongodb.conf
+#sudo service mongodb restart
