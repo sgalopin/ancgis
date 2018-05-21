c4c4e96979c27372a856753232d23fbfc7874aaa
sgalopin <sg@griffedartistes.com>
2018-05-21 11:22:20 +0200

    travis conf

--- shell/populate-db.sh
+++ shell/populate-db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -e
-set -o pipefail
+# set -e
+# set -o pipefail
+
 echo "--------------------------------------------------"
 echo " Populate DB "
 echo "--------------------------------------------------"
+
 cd /var/tmp/anc/database
 echo "--- Populating of the taxons collection."
 mongo localhost/anc taxons.js
--- shell/setup-vm.sh
+++ shell/setup-vm.sh
@@ -6,15 +6,15 @@ echo "--------------------------------------------------"
 echo " Setup the VM "
 echo "--------------------------------------------------"
 
-# Port 80 requires elevated privileges
+echo " Port 80 requires elevated privileges "
 # https://docs.requarks.io/wiki/troubleshooting#error-listening-on-port-xx-requires-elevated-privileges
 sudo apt-get install -y libcap2-bin
 sudo setcap 'cap_net_bind_service=+ep' `which node`
 
-# MongoDB
+echo " MongoDB "
 sudo apt-get install -y mongodb
-sed -i 's/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g' /etc/mongodb.conf
-service mongodb restart
+sudo sed -i 's/bind_ip = 127.0.0.1/#bind_ip = 127.0.0.1/g' /etc/mongodb.conf
+sudo service mongodb restart
 
-# Setup the database
-/bin/bash ./populate-db.sh
+echo " Setup the database "
+/bin/bash populate-db.sh
