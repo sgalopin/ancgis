ae36ee4270ced9a8a84d068f902200ce65237f88
sgalopin <sg@griffedartistes.com>
2018-09-11 23:43:15 +0200

    updating cache file

--- application/public/cache.js
+++ application/public/cache.js
@@ -3,14 +3,17 @@ self.addEventListener('install', function(event) {
     caches.open("ancgis-statics-ressources").then(function(cache) {
       return cache.addAll(
         [
+          '/',
           '/manifest.json',
-          'https://openlayers.org/en/v4.6.5/css/ol.css',
-          'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
           './stylesheets/style.css',
+          '/login',
+          '/rest/taxons',
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
           'https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL',
-          'https://openlayers.org/en/v4.6.5/build/ol-debug.js',
-          'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
-          'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'
+          'https://code.jquery.com/jquery-3.3.1.min.js',
+          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'
+
         ]
       );
     })
