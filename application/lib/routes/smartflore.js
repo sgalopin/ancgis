fa0f3edb38659a037da7f67554a17708d03acfc1
sgalopin <sg@griffedartistes.com>
2020-06-08 03:27:24 +0200

    Small refactorings

--- application/lib/routes/smartflore.js
+++ application/lib/routes/smartflore.js
@@ -37,13 +37,17 @@ router.get("/:urn", function (req, res) {
   // https://fr.wikipedia.org/wiki/
   // https://inpn.mnhn.fr/espece/cd_nom/
 
+  const url = 'https://www.tela-botanica.org/eflore/consultation/index_mobile.php?module=mobile&referentiel=bdtfx&num_nom=' + req.params.urn;
+  console.log('URL %j', url);
+
   request({
-      url: 'https://www.tela-botanica.org/eflore/consultation/index_mobile.php?module=mobile&referentiel=bdtfx&num_nom=' + req.params.urn,
+      url: url,
       proxy: proxy
   }, function (error, response, body) {
       if (error) {
           console.log(error);
       } else {
+        console.log('Response obtained. Data processing...');
         const re = new RegExp('modules/mobile/presentations/', 'g');
         const content = response.body.replace(re, 'https://tela-botanica.org/eflore/consultation/modules/mobile/presentations/')
                         .replace('</head>', '<link href="/stylesheets/smartflore.css" type="text/css" rel="stylesheet"></head>');
